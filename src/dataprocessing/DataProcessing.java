e5b00e6e0146956e2ec98b9e983c1aaf1f3e601f
rf922 <rfabiani@sfsu.edu>
2024-01-19 23:07:39 -0800

    refactoring Main to seperate csv parsing logic to MigrationDataUtils

--- src/dataprocessing/DataProcessing.java
+++ src/dataprocessing/DataProcessing.java
@@ -33,73 +33,8 @@ public class DataProcessing {
     public static void main(String[] args) {
         String dataSet = "data/migrantsMissing08112021.csv";
         String malformedData = "malformedEntries.csv";
-
-        int threadCount = Runtime.getRuntime().availableProcessors();
-        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
-        CompletionService<MigrationData> completionService = new ExecutorCompletionService<>(executor);
-        List<MigrationData> dataList = new LinkedList<>();
-        List<String> malformedList = new ArrayList<>();
-
-        try (BufferedReader csvReader = new BufferedReader(new FileReader(dataSet))) {
-            String header = csvReader.readLine();
-            int taskCount = 0;
-
-            for (String line; (line = csvReader.readLine()) != null;) {
-
-                String finalLine = line;
-                completionService.submit(() -> MigrationData.parseLine(finalLine));
-                taskCount++;
-            }
-
-            for (int i = 0; i < taskCount; i++) {
-                Future<MigrationData> future = completionService.take();
-                dataList.add(future.get());
-            }
-
-            Map<String, List<MigrationData>> incidentsByCountry
-                = MigrationDataUtils.groupByField(dataList, MigrationData::incidentCountry);
-            Map<Integer, List<MigrationData>> incidentsByYear = MigrationDataUtils.groupByField(dataList, x -> x.incidentDate().getYear());
-            
-            int deaths = incidentsByCountry.get("Mexico").stream().mapToInt(x -> x.numberDead()).sum();
-            
-            incidentsByYear.forEach((x, y) -> {
-                
-                Integer yearDeathTotal = y.stream().mapToInt(entry -> entry.numberDead()).sum();
-                Integer yearMissingTotal = y.stream().mapToInt(entry -> entry.numberMissing()).sum();
-                Integer womenD = y.stream().mapToInt(entry -> entry.numberWomen()).sum();
-                String sf = String.format("%s\n%d\n%s\t%d\n%s\t%d\n%s", 
-                    "--------------------------------------------------",
-                    x, 
-                    "Death Total : ",yearDeathTotal,
-                    "Total Dissappearances : ", yearMissingTotal,
-                    
-                    "--------------------------------------------------");
-                
-                System.out.println(sf);
-            });
-            
-            System.out.println(deaths);
-
-        } catch (Exception ex) {
-            System.out.println(ex);
-        } finally {
-            executor.shutdown();
-            System.out.println(" [ MAIN ] : DataList Size " + dataList.size());
-            System.out.println(" [ MAIN ] : Malformed Size " + malformedList.size());
-
-            /**
-             * DEBUGGING entries long lackingFields =
-             * malformedList.stream().filter(x -> x.split(";", -1).length < 23).count();
-             * long surplusFields = malformedList.size() - lackingFields;
-             * System.out.println("[ MAIN ] : Lacking Fields "+lackingFields);
-             * System.out.println("[ MAIN ] : Surplus Fields "+surplusFields);
-             * malformedList.stream().filter(x -> x.split(";", -1).length >
-             * 23).map(x -> x.split(";",
-             * -1).length).distinct().forEach(System.out::println);
-             * malformedList.stream().filter(x -> x.split(";", -1).length >
-             * 23).limit(15).forEach(System.out::println);
-             */
-        }
+        List<MigrationData> dataList = MigrationDataUtils.loadDataFromCSV(dataSet);
+        System.out.println(dataList.size());
     }
 
 }
--- src/dataprocessing/MigrationDataUtils.java
+++ src/dataprocessing/MigrationDataUtils.java
@@ -5,27 +5,65 @@
  */
 package dataprocessing;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.function.Function;
 
 /**
  * Various utilities for making different operations on the data
+ *
  * @author rf922
  */
 public class MigrationDataUtils {
 
+    public static List<MigrationData> loadDataFromCSV(String dataSet) {
+        List<MigrationData> dataList = new LinkedList<>();
+        int threadCount = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        CompletionService<MigrationData> completionService = new ExecutorCompletionService<>(executor);
+        
+        try (BufferedReader csvReader = new BufferedReader(new FileReader(dataSet))) {
+            int taskCount = 0;
+            
+            for (String line; (line = csvReader.readLine()) != null;) {
+                String finalLine = line;
+                completionService.submit(() -> MigrationData.parseLine(finalLine));
+                taskCount++;
+            }
+
+            for (int i = 0; i < taskCount; i++) {
+                Future<MigrationData> future = completionService.take();
+                dataList.add(future.get());
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } finally {
+            executor.shutdown();
+        }
+
+        return dataList;
+    }
+
     /**
-     * Method to collect and group data entries by a field. Accepts a list of 
-     * migration data entries, a function for key extraction. The function returns a map
-     * of keys and a list of corresponding migration data entries.
-     * 
+     * Method to collect and group data entries by a field. Accepts a list of
+     * migration data entries, a function for key extraction. The function
+     * returns a map of keys and a list of corresponding migration data entries.
+     *
      * @param <K>
      * @param dataList
      * @param keyExtractor
-     * @return 
+     * @return
      */
     public static <K extends Comparable<K>> Map<K, List<MigrationData>> groupByField(
         List<MigrationData> dataList,
